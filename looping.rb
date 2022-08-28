4ada7dd5c231018087386500a794b49b595cc886
Allen Mordukhaev <allenmor93@gmail.com>
2022-08-28 18:32:34 -0400

    commit tree dfbeec13732e6461b359de6a08f765089a0f4dcc

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def happy_new_year
   # your code here
+  (1..10).reverse_each do |num|
+    puts num
+  end
+  puts 'Happy New Year!'
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +21,19 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz num
+  end
 end
+
 
 def reverse_string(str)
   # your code here
+  newWord = ''
+  str.each_char do |letter|
+    newWord << letter
+  end
+  newWord.chars.reduce { |x, y| y + x } 
 end
+
+reverse_string('manhattan')
